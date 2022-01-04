4c73a75702820b959ac4c8062180a85616e93784
Diego Orcí <diegoorcibs@gmail.com>
2022-01-04 14:34:34 -0600

    Update productos.js

--- routes/productos.js
+++ routes/productos.js
@@ -19,4 +19,56 @@ router.get("/SelectProducto",(req,res)=>{
         }
     });
 });
+
+router.get("/SelectoUnProducto:id"), (req,res)=>{
+    mysqlConnection.query("SELECT * FROM productos WHERE id = ?"), [req.params.id],(err,rows)=>{
+        if(err){
+            res.json({
+                mensaje:"Error al mostrar los productos"
+            });
+        }else{
+            res.json(rows)
+        }
+    }
+}
+
+router.post("/AgregarProducto",(req,res) =>{
+    let data = {nombre:req.body.nombre,descripcion:req.body.descripcion,precio:req.body.precio,stock:req.body.stock}
+    let sql = "INSERT INTO productos SET ?"
+    mysqlConnection.query(sql,data,function(err,result){
+        if(err){
+            throw err
+        }else{
+            Object.assign(data,{id:result.isertId})
+            res.send(data)
+        }
+    })
+})
+
+router.put("/EditarProducto:id",(req,res) =>{
+    let id = req.params.id
+    let nombre = req.body.nombre
+    let descripcion = req.body.descripcion
+    let precio = req.body.precio
+    let stock = req.body.stock
+    let sql = "UPDATE productos SET nombre = ?, descripcion = ?, precio = ?, stock = ? WHERE id = ?"
+    mysqlConnection.query(sql,[nombre,descripcion,precio,stock,id], function(err,results){
+        if(err){
+            throw err
+        }else{
+            res.send(results)
+        }
+    })
+})
+
+router.delete("/EliminarProducto:id",(req,res)=>{
+    mysqlConnection.query("DELETE FROM productos WHERE id = ?",[req.params.id],function(err,rows){
+        if(err){
+            throw err
+        }else{
+            res.send(rows)
+        }
+    })
+})
+
 module.exports=router;
