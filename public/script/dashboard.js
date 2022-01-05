c8a24b5f2347c6db7b942958f79eb44db17c5952
Diego Orcí <74203805+Bold98@users.noreply.github.com>
2022-01-04 21:49:03 -0700

    fin del crud

--- public/script/dashboard.js
+++ public/script/dashboard.js
@@ -1,3 +1,4 @@
+const url = 'http://localhost:3000/productos/'
 const contenedor = document.querySelector('tbody')
 let resultados = ''
 
@@ -36,7 +37,93 @@ const mostrar = (productos) => {
     contenedor.innerHTML = resultados
 }
 //Mostrar
-fetch('http://localhost:3000/productos/SelectProducto')
+fetch(url)
     .then(response => response.json())
     .then(data => mostrar(data))
     .catch(error => console.log(error))
+
+const on = (element, event, selector, handler) => {
+    element.addEventListener(event, e => {
+        if (e.target.closest(selector)) {
+            handler(e)
+        }
+    })
+}
+//Procedimiento borrar
+on(document, 'click', '.btnBorrar', e => {
+    const fila = e.target.parentNode.parentNode
+    const id = fila.firstElementChild.innerHTML
+    alertify.confirm("This is a confirm dialog.",
+        function () {
+            fetch('http://localhost:3000/productos/' + id, {
+                method: 'DELETE'
+            })
+                .then(res => res.json())
+                .then(() => location.reload())
+            //alertify.success('Ok')
+        },
+        function () {
+            alertify.error('Cancel')
+        });
+})
+
+//Procedimiento Editar
+let idForm = 0
+on(document, 'click', '.btnEditar', e => {
+    const fila = e.target.parentNode.parentNode
+    idForm = fila.children[0].innerHTML
+    const nombreForm = fila.children[1].innerHTML
+    const descripcionForm = fila.children[2].innerHTML
+    const precioForm = fila.children[3].innerHTML
+    const stockForm = fila.children[4].innerHTML
+    nombre.value = nombreForm
+    descripcion.value = descripcionForm
+    precio.value = precioForm
+    stock.value = stockForm
+    opcion = 'editar'
+    modalProductos.show()
+})
+
+//Procedimiento crear y editar
+
+formArticulo.addEventListener('submit', (e) => {
+    e.preventDefault()
+    if (opcion == 'crear') {
+        fetch('http://localhost:3000/productos/AgregarProducto', {
+            method:'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                nombre: nombre.value,
+                descripcion: descripcion.value,
+                precio: precio.value,
+                stock: stock.value
+            })
+        })
+            .then(response => response.json())
+            .then(data => {
+                const nuevoProducto = []
+                nuevoProducto.push(data)
+                mostrar(nuevoProducto)
+            })
+        
+    }
+    if (opcion == 'editar') {
+        fetch('http://localhost:3000/productos/'+idForm, {
+            method:'PUT',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                nombre: nombre.value,
+                descripcion: descripcion.value,
+                precio: precio.value,
+                stock: stock.value
+            })
+        })
+        .then(response => response.json())
+        .then(response => location.reload())
+    }
+    modalProductos.hide()
+})
--- routes/productos.js
+++ routes/productos.js
@@ -1,74 +1,76 @@
-const axios = require('axios');
-const express = require('express');
-const request  = require('request');
-const router = express.Router();
-const path = require ("path");
-const mysqlConnection = require("../conexionDB");
-const { response } = require('express');
-const { url } = require('inspector');
-const { header } = require('express/lib/request');
+const axios = require('axios')
+const express = require('express')
+const request = require('request')
+const router = express.Router()
+const path = require("path")
+const mysqlConnection = require("../conexionDB")
+const { response } = require('express')
+const { url } = require('inspector')
+const { header } = require('express/lib/request')
 
-router.get("/SelectProducto",(req,res)=>{
-    mysqlConnection.query("SELECT * FROM productos",(err,rows,fields )=>{
-        if(!err){
-            res.json(rows);
-        }else{
+router.get("/", (req, res) => {
+    mysqlConnection.query("SELECT * FROM productos", (err, rows, fields) => {
+        if (!err) {
+            res.json(rows)
+        } else {
             res.json({
-                mensaje:"Error al mostrar los productos"
+                mensaje: "Error al mostrar los productos"
             });
         }
     });
 });
 
-router.get("/SelectoUnProducto:id"), (req,res)=>{
-    mysqlConnection.query("SELECT * FROM productos WHERE id = ?"), [req.params.id],(err,rows)=>{
-        if(err){
+/*router.get("/SelectoUnProducto:id"), (req, res) => {
+    mysqlConnection.query("SELECT * FROM productos WHERE id = ?"), [req.params.id], (err, rows) => {
+        if (err) {
             res.json({
-                mensaje:"Error al mostrar los productos"
+                mensaje: "Error al mostrar los productos"
             });
-        }else{
+        } else {
             res.json(rows)
         }
     }
-}
+}*/
 
-router.post("/AgregarProducto",(req,res) =>{
-    let data = {nombre:req.body.nombre,descripcion:req.body.descripcion,precio:req.body.precio,stock:req.body.stock}
+router.post("/AgregarProducto", (req, res) => {
+    let data = { nombre: req.body.nombre, descripcion: req.body.descripcion, precio: req.body.precio, stock: req.body.stock }
     let sql = "INSERT INTO productos SET ?"
-    mysqlConnection.query(sql,data,function(err,result){
-        if(err){
+    mysqlConnection.query(sql, data, function (err, result) {
+        if (err) {
             throw err
-        }else{
-            Object.assign(data,{id:result.isertId})
+        } else {
+            Object.assign(data, { id: result.isertId })
             res.send(data)
         }
     })
 })
 
-router.put("/EditarProducto:id",(req,res) =>{
+router.put("/:id", (req, res) => {
     let id = req.params.id
     let nombre = req.body.nombre
     let descripcion = req.body.descripcion
     let precio = req.body.precio
     let stock = req.body.stock
-    let sql = "UPDATE productos SET nombre = ?, descripcion = ?, precio = ?, stock = ? WHERE id = ?"
-    mysqlConnection.query(sql,[nombre,descripcion,precio,stock,id], function(err,results){
-        if(err){
+    //let images = req.body.images
+    let sql = "UPDATE productos SET nombre = ?, descripcion = ?, precio = ?, stock = ? WHERE idProducto = ?"
+    mysqlConnection.query(sql, [nombre, descripcion, precio, stock, id], function (err, results) {
+        if (err) {
             throw err
-        }else{
+        } else {
             res.send(results)
         }
     })
 })
 
-router.delete("/EliminarProducto:id",(req,res)=>{
-    mysqlConnection.query("DELETE FROM productos WHERE id = ?",[req.params.id],function(err,rows){
-        if(err){
+router.delete("/:id", (req, res) => {
+    const { id } = req.body;
+    mysqlConnection.query("DELETE FROM productos WHERE idProducto = ?", [req.params.id], function (err, rows) {
+        if (err) {
             throw err
-        }else{
+        } else {
             res.send(rows)
         }
     })
 })
 
-module.exports=router;
+module.exports = router;
